d0afe9d24bafa7f53dbd01d11f44f34809e57085
JojiKoike <joji.koike@gmail.com>
2020-04-28 22:43:43 +0900

    GitHub認証の実装完了

--- app.js
+++ app.js
@@ -3,11 +3,44 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var helmet = require('helmet');
+var session = require('express-session');
+var passport = require('passport');
 
 var app = express();
+
+// Security Enhancement
+app.use(helmet());
+
+// GitHub OAuth Authentication
+var GitHubStrategy = require('passport-github2').Strategy;
+var config = app.get('env') === "development"? require('./config.json') : ""
+var GITHUB_CLIENT_ID = app.get('env') === "development"? config.GITHUB_CLIENT_ID : process.env.GITHUB_CLIENT_ID;
+var GITHUB_CLIENT_SECRET = app.get('env') === "development"? config.GITHUB_CLIENT_SECRET : process.env.GITHUB_CLIENT_SECRET;
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://192.168.33.10:8000/auth/github/callback'
+},(accessToken, refreshToken, profile, done) => {
+    process.nextTick(() => {
+      return done(null, profile);
+    });
+  }
+));
+
+// Router
+var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +52,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: app.get('env') === "development" ? config.GITHUB_SESSION_SECRET : process.env.GITHUB_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email']}),
+  (req, res) => {}
+)
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => { res.redirect('/'); }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
